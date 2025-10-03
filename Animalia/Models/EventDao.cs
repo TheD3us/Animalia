f164f5ac44da9c7e310611747aa6ca7b94af00a6
Mikael Heyraud <mikaelhey@hotmail.fr>
2025-10-03 15:09:26 +0200

    DAO et Controller de Events et ProgramModel

---
+++ Animalia/Models/EventDao.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Animalia.Models
+{
+    public class EventDao
+    {
+        public List<Events> SelectAll()
+        {
+            AnimaliaDbEntities context = new AnimaliaDbEntities();
+            return context.Events.Include(e => e.Users).ToList();
+        }
+
+        public Events Select(int id)
+        {
+            AnimaliaDbEntities context = new AnimaliaDbEntities();
+            return context.Events.Where(e => e.Id == id).First();
+        }
+
+        public void Input(Events evt)
+        {
+            AnimaliaDbEntities context = new AnimaliaDbEntities();
+            context.Events.Add(evt);
+            context.SaveChanges();
+        }
+
+        public void Put(Events evt)
+        {
+            AnimaliaDbEntities context = new AnimaliaDbEntities();
+            context.Entry(evt).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            AnimaliaDbEntities context = new AnimaliaDbEntities();
+            Events evt = context.Events.Where(e => e.Id == id).First();
+            context.Events.Remove(evt);
+            context.SaveChanges();
+        }
+    }
+}
