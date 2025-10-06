68cadc23c70287f25efcee1697665ad3ce7f993b
Mezier <50947908+TheD3us@users.noreply.github.com>
2025-10-06 11:17:16 +0200

    ajout service user et training

--- animalia-front/src/app/services/training-service.ts
+++ animalia-front/src/app/services/training-service.ts
@@ -1,8 +1,55 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environement/environment';
+import { training } from '../Model/training';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrainingService {
+  constructor(private http: HttpClient){}
   
+  getListe(){
+    return this.http.get(environment.apiUrl + "training");
+  }
+
+  get(id: number){
+    return this.http.get(environment.apiUrl + "training/" + id);
+  }
+
+  post(data: training){
+    const body = JSON.stringify(data);
+
+    this.http.post(environment.apiUrl + "training", body, {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    }).
+      subscribe(response => {
+
+        console.log("crud service post training OK");
+
+      });
+  }
+
+  delete(id: number){
+    this.http.delete(environment.apiUrl + "training/" + id).subscribe(response => {
+
+        console.log("crud service delete training OK");
+
+      });
+  }
+
+  put(data: training){
+    const body = JSON.stringify(data);
+    this.http.put(environment.apiUrl + "training", body, {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    }).subscribe(response => {
+
+        console.log("crud service put training OK");
+
+      });
+  }
 }
--- animalia-front/src/app/services/user-service.ts
+++ animalia-front/src/app/services/user-service.ts
@@ -1,8 +1,55 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environement/environment';
+import { user } from '../Model/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  constructor(private http: HttpClient){}
   
+  getListe(){
+    return this.http.get(environment.apiUrl + "user");
+  }
+
+  get(id: number){
+    return this.http.get(environment.apiUrl + "user/" + id);
+  }
+
+  post(data: user){
+    const body = JSON.stringify(data);
+
+    this.http.post(environment.apiUrl + "user", body, {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    }).
+      subscribe(response => {
+
+        console.log("crud service post user OK");
+
+      });
+  }
+
+  delete(id: number){
+    this.http.delete(environment.apiUrl + "user/" + id).subscribe(response => {
+
+        console.log("crud service delete user OK");
+
+      });
+  }
+
+  put(data: user){
+    const body = JSON.stringify(data);
+    this.http.put(environment.apiUrl + "user", body, {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    }).subscribe(response => {
+
+        console.log("crud service put user OK");
+
+      });
+  }
 }
